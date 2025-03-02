e5040132f93f5348c4d982d852e59ccd86f6c915
Albie <albievanags@gmail.com>
2025-03-02 18:44:31 +0000

    added top part to all pages

--- src/scripts/lets-talk.js
+++ src/scripts/lets-talk.js
@@ -1,1 +1,13 @@
+import { CoverPhoto } from './cover-photo';
+import { NavBar } from './nav-bar';
+
 import '../styles/lets-talk.scss';
+
+const topPartHeight = '75vw';
+const topPartMaxHeight = '35vh'
+
+const navBar = new NavBar(topPartHeight, topPartMaxHeight);
+navBar.load();
+
+const coverPhoto = new CoverPhoto('Paul Vanags', 'Let\'s Talk!', 'assets/images/books-cover-photo.jpg', 'books-cover-photo', false, topPartHeight, topPartMaxHeight, '10rem');
+coverPhoto.load();
--- src/scripts/publications.js
+++ src/scripts/publications.js
@@ -1,1 +1,13 @@
+import { CoverPhoto } from './cover-photo';
+import { NavBar } from './nav-bar';
+
 import '../styles/publications.scss';
+
+const topPartHeight = '75vw';
+const topPartMaxHeight = '35vh'
+
+const navBar = new NavBar(topPartHeight, topPartMaxHeight);
+navBar.load();
+
+const coverPhoto = new CoverPhoto('Paul Vanags', 'Publications', 'assets/images/books-cover-photo.jpg', 'books-cover-photo', false, topPartHeight, topPartMaxHeight, '10rem');
+coverPhoto.load();
